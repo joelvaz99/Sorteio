65b2c410caf76328090f6222dd76c95379545f24
joelvaz99 <joelvaz@ipvc.pt>
2020-10-10 16:00:19 +0100

    Aplicativo de sorteio

---
+++ app/src/main/java/com/e/sorteio/MainActivity.java
@@ -0,0 +1,30 @@
+package com.e.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View v){
+
+
+        TextView texto = findViewById(R.id.textResultado);
+
+        int x = new Random().nextInt(11);
+
+        texto.setText("Numero: " +x);
+
+
+    }
+}
